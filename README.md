96079f81196d4a0fb06635656dffeaeef5f2f437
Rajadi01 <rajputadityasingh23cs@rnsit.ac.in>
2025-02-20 10:17:31 +0530

    day3 ka kaam

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Samsung-
-This repo is created for Samsung Innovation Campus python course
+# rnsit_sam_py
+This Repo is created for the Samsung Python data science training in the month of feb 2025
--- day2/bigger.py
+++ day2/bigger.py
@@ -1,5 +1,6 @@
 number1 = input("Enter the number: ")
 list1 = []
+list2=[]
 for i in number1:
     list1.append(i)
 list1.reverse()
@@ -7,4 +8,7 @@ number2 = "".join(list1)
 if int(number1) < int(number2):
     print(number2, "is the second smallest bigger number")
 else:
-    print("no number exist")
+    for i in len(number1):
+        for j in range(2**(len(number1)-1)):
+            list2.append(number1[i]+number1[j]+number1[j+1])
+    
