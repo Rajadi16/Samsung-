3cd3d7ee8fe63358e2dabde5e32dedcbf511b97a
Rajadi16 <rajadi7599@gmail.com>
2025-02-25 16:20:07 +0530

    hackthon project

---
+++ day7/C_Section.py
@@ -0,0 +1,12 @@
+gender=input("apka Ling m/w:")
+
+if gender=="w":
+    month=int(input("Apka konsa mahina h"))
+    chut=int(input("Chut Ka size Batao"))
+    if chut>=10:
+        print("normal delivery!!")
+    else:
+        print("apke toh C lagege C Section")
+else:
+    print("apke lode lagege")
+    
